6771a965083b222be7f9fff26950d16b060c3ce2
Tristan Long <longtr@oregonstate.edu>
2023-03-20 07:35:49 -0400

    Implemented breadth first search

--- breadth_first_search.py
+++ breadth_first_search.py
@@ -1,13 +1,35 @@
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["anuj"] = []
+graph["claire"] = ["thom", "jonny"]
+
+from collections import deque
+search_queue = deque()
+search_queue += graph["you"] 
+
+def person_is_seller(name):
+    return name[-1] == 'm'
+
 def search(name):
     search_queue = deque()
     search_queue += graph[name]
     searched = []
     while search_queue:
         person = search_queue.popleft()
-        if not person_is_seller(person):
-            print(person)
-            return True
-        else:
-            search_queue += graph[person]
-            searched.append(person)
+        if not person in searched:
+            if person_is_seller(person):
+                print(person + " is a mango seller!")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
     return False
